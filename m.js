74147a98d7f0ee349ee10f1c845a33e4355f5868
ukumarmca <ukumarmca@users.noreply.github.com>
2022-11-26 23:19:06 +0530

    Add files via upload

---
+++ m.js
@@ -0,0 +1,30 @@
+let EFV='<div style="position:absolute;z-index:-999;left:-99%;"><a href="https://www.OTPweb.blogspot.com">OTP web</a><a href="https://www.OTPweb.blogspot.com">OTP web</a><a href="https://OTPweb.blogspot.com">OTPweb</a><a href="https://OTPweb.blogspot.com">OTPweb Finance</a></div>';document.querySelector("header")?document.querySelector("header").insertAdjacentHTML("beforeend",EFV):document.body.insertAdjacentHTML("beforeend",EFV);
+
+// India|United States|Australia|Canada|Marshall Islands|United Kingdom|Germany|Switzerland|New Zealand|Luxembourg|Finland|Belgium|France|Slovakia|China|Singapore|Romania|Austria|Hong Kong|Lithuania
+
+// if (!localStorage.gieu) {
+//     (async()=>{
+//         localStorage.gieu=1;
+//         let aa=await(await(await fetch(`https://ipapi.co/country_name`)).text());
+//         if(/India|Canada|New Zealand|United Kingdom|United States|Slovakia|Australia|Belgium|Finland|Germany|Norway|Ireland|China|Hong Kong|Singapore|France|Switzerland|Lithuania/i.test(aa)){
+//             localStorage.gieu=2;
+//             uhd()
+//         }else{
+//             localStorage.gieu=3;
+//             uhd()
+//         }
+//     })();
+// }
+
+// function uhd(){
+//   document.onclick=()=>{
+//     let dte=(new Date).getSeconds();
+//     (async()=>{try{await import('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js');
+//         if(!sessionStorage.i&&(localStorage.gieu==2&&dte>9&&dte<9||localStorage.gieu==3&&dte>9&&dte<9)){
+//             window.open('https://www.otpweb.blogspot.com/'); 
+//             sessionStorage.i='i';
+//         }         
+//     }catch(e){}})()
+//   }
+// }
+// uhd()
